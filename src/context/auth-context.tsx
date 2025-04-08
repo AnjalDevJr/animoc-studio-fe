37490cdbe785d4f6b8493763dbbd8a79b82bd3ce
anjal <anjalthapa578@gmail.com>
2025-04-08 22:18:01 +0545

    created auth context hook for loggedInUser

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,21 +1,58 @@
-import { createContext } from "react";
+import { get } from "@/config/axios.config";
+import { useQuery } from "@tanstack/react-query";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getLoggedInUser = async () => {
+    try {
+      const token = localStorage.getItem("token") || null;
+      if (token) {
+        const response: any = await get("/auth/me", {
+          headers: {
+            // prettier-ignore
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+          },
+        });
+        setData(response.detail);
+      }
+    } catch (exception) {
+      throw exception;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, []);
+
+  // const { data, isLoading, error } = useGetLoggedInUser() as {
+  //   data: any;
+  //   isLoading: boolean;
+  //   error: any;
+  // };
+
+  // console.log({ data, isLoading, error });
+
+  if (isLoading) return <>Loading...</>;
+
   return (
     <>
       <AuthContext.Provider
         value={{
-          loggedInUser: null,
-          setLoggedInUser: null,
+          loggedInUser: data || null,
+          setLoggedInUser: setData,
         }}
       >
         {children}
       </AuthContext.Provider>
-      ;
     </>
   );
 };
